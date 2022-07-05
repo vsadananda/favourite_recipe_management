e5a16d9cddaf820a2d4dccfea90ee8cf367c7fb4
vsadananda <107986968+vsadananda@users.noreply.github.com>
2022-07-06 00:41:43 +0530

    Update RecipeInternalException.java

--- src/main/java/com/favourite/recipe/exception/RecipeInternalException.java
+++ src/main/java/com/favourite/recipe/exception/RecipeInternalException.java
@@ -1,10 +1,3 @@
-/*Copyright © 2019 Robert Bosch Engineering and Business Solutions Private Limited. All Rights Reserved.
-
-NOTICE:  All information contained herein is, and remains the property of Robert Bosch Engineering and Business Solutions Private Limited.
-Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from 
-Robert Bosch Engineering and Business Solutions Private Limited.
-
-*/
 package com.favourite.recipe.exception;
 
 public class RecipeInternalException extends RecipeException {
